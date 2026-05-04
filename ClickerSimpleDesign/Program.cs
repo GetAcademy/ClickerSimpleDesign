ca2bdbe85e70ff3680b25a92d2d9425abf8e5f17
GET Academy <terje@kolderup.net>
2026-05-04 14:21:04 +0200

    Initial commit

---
+++ ClickerSimpleDesign/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+var count = 0;
+
+var fileName = "clicker.data";
+if (File.Exists(fileName))
+{
+    var countStr = File.ReadAllText(fileName);
+    count = int.Parse(countStr);
+}
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine(count);
+    Console.WriteLine("<trykk på space for poeng - eller ESC for avslutt>");
+
+    var key = Console.ReadKey();
+    if (key.Key == ConsoleKey.Spacebar)
+    {
+        count++;
+        File.WriteAllText(fileName, "" + count);
+    }
+    else if (key.Key == ConsoleKey.Escape)
+    {
+        return;
+    }
+}
