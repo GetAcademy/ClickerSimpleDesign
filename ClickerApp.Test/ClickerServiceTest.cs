ee4df432280769e7a79232ef06d97185797394be
GET Academy <terje@kolderup.net>
2026-05-04 14:40:08 +0200

    x

---
+++ ClickerApp.Test/ClickerServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using ClickerApp.Core;
+
+namespace ClickerApp.Test
+{
+    public class ClickerServiceTest
+    {
+        [Test]
+        public void TestIncrement()
+        {
+            // arrange
+            var clicker = new Clicker("7");
+
+            // act
+            var result = ClickerService.ProcessKey(ConsoleKey.Spacebar, clicker);
+
+            // assert
+            Assert.That(clicker.Count, Is.EqualTo(8));
+            Assert.That(result, Is.EqualTo(ClickerResult.Incremented));
+        }
+    }
+}
